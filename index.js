7a05c3cfca3dc92f4c4844e91457648defb2267a
AngomRanjan <angomranjan@gmail.com>
2022-06-07 02:14:31 +0530

    Add Luxon Time

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import displayList from './modules/abs_booklist.js';
+import { DateTime } from './modules/luxon.js';
+
+function startTime() {
+const newDateTime = DateTime.now().toLocaleString(DateTime.DATE_FULL) + ', ' + DateTime.now().toLocaleString(DateTime.TIME_24_WITH_SECONDS);
+const time = document.getElementById('time');
+time.textContent = newDateTime;
+  setTimeout(() => {
+    startTime();
+  }, 500);
+}
+startTime();
 
 displayList();
