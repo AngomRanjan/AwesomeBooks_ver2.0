f2b802bb469b83809b738e1c700a24071798e0d8
AngomRanjan <angomranjan@gmail.com>
2022-06-07 07:19:27 +0530

    Create Contact Module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import displayList from './modules/abs_booklist.js';
 import showTime from './modules/abs_time.js';
 import add from './modules/abs_add.js';
+import displayContact from './modules/asb_contact.js';
 
 showTime();
 displayList();
@@ -17,5 +18,11 @@ document.getElementById('list').addEventListener('click', (event) => {
 document.getElementById('add').addEventListener('click', (event) => {
   reset();
   add();
+  event.stopPropagation();
+});
+
+document.getElementById('contact-us').addEventListener('click', (event) => {
+  reset();
+  displayContact();
   event.stopPropagation();
 });
